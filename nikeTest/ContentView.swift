daaff790a22f62dcced3a6c808bc4b893c6a6e1a
Rossignol Alex <alexnw33910@gmail.com>
2024-12-12 12:34:30 +0100

    carousel shoes

--- nikeTest/ContentView.swift
+++ nikeTest/ContentView.swift
@@ -8,34 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var userBool: Bool = true
-
+    @State private var index: Int = 0
+    @State public var userCart: Double
+    = 0.0
+    @State public var nbCart: Int = 0
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
-                Image(userBool ? "nikeleg" : "nikewhite")
+                Text("Nike Store")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                HStack{
+                    Text("Basket: \(userCart, specifier: "%.2f")$")
+                        .font(.headline)
+                        .padding(.trailing, -4.0)
+                        .padding(.leading, 8.0)
+                    Text("(\(nbCart))")
+                        .font(.footnote)
+                        .fontWeight(.ultraLight)
+                        .multilineTextAlignment(.leading)
+                        .padding([.bottom, .trailing], 12.0)
+                    
+                }
+                
+                Spacer()
+                
+                Image(shoes[index].picture)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 300.0, height: 300.0)
                     .opacity(0.8)
                     .cornerRadius(32)
                     .transition(.opacity)
-                    .animation(.easeInOut(duration: 1.0), value: userBool)
+                    .animation(.easeInOut(duration: 1.0), value: index)
                     
-                Text(userBool ? "Old Nike - 120.20$" : "New Nike - 78.30$")
+                Text("\(shoes[index].name) - \(shoes[index].price, specifier: "%.2f")$")
                     .font(.headline)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.center)
                     .padding()
-                Spacer()
-                Button("See other shoes") {
-                    userBool = !userBool
+                     
+                Button("Buy"){
+                    userCart += shoes[index].price
+                    nbCart += 1
                 }
+                .buttonStyle(.bordered)
+                
+                Spacer()
+                HStack{
+                    Button("Previous shoes") {
+                        if index > 0 {
+                            index -= 1
+                        } else {
+                            index = shoes.count - 1
+                        }
+                    }
+                    Button("Next shoes") {
+                        if index < shoes.count - 1 {
+                            index += 1
+                        } else {
+                            index = 0
+                        }
+                    }
+                }
+                .buttonStyle(.borderedProminent)
                 .padding(.all)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
+                
                 Spacer()
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
---
+++ nikeTest/data.swift
@@ -0,0 +1,21 @@
+//
+//  data.swift
+//  nikeTest
+//
+//  Created by Alex Rossignol on 12/12/2024.
+//
+
+import Foundation
+
+struct Shoe {
+    var name: String
+    var picture: String
+    var price: Double
+}
+
+var shoes = [
+    Shoe(name: "Air Force One", picture: "forceone", price: 120.90),
+    Shoe(name: "Nike Air Max", picture: "airmax", price: 200.0),
+    Shoe(name: "Air Jordan", picture: "jordan", price: 125.45),
+    Shoe(name: "Nike Dunk", picture: "dunk", price: 225.10)
+]
